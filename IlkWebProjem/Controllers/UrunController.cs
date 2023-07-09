36626fa351c2b42f34ccb9452dc3892df3359407
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-07-09 11:49:03 +0300

    Urun Controller hazırlandı.

---
+++ IlkWebProjem/Controllers/UrunController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IlkWebProjem.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
