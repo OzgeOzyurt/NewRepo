b8ec379a582c18d378a07a267d966809805013e1
OzgeOzyurt <ozgeozyurt00@gmail.com>
2023-07-09 10:51:46 +0300

    first checkin

---
+++ IlkWebProjem/Controllers/PersonelController.cs
@@ -0,0 +1,17 @@
+﻿using IlkWebProjem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlkWebProjem.Controllers
+{
+    public class PersonelController : Controller
+    {
+        public IActionResult Index()
+        {
+            Personel p1 = new Personel() { Ad = "Selami", Soyad = "Şahin", Departman = "Müzik", Aktif = true };
+
+            return View(p1);
+        }
+    }
+}
+
+//www.hepsiburada.com/personel/index
